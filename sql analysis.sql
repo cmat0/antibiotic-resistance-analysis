5df54acf8ebab59c16174a9d10e3ba4ea863a5c5
cmat0 <c.mat0@outlook.com>
2026-09-06 21:43:40 +0100

    joined files using SQL

--- sql analysis.sql
+++ sql analysis.sql
@@ -2,8 +2,6 @@ CREATE DATABASE antibiotics_data;
 
 USE antibiotics_data;
 
-SET GLOBAL local_infile = 1;
-
 CREATE TABLE consumption_location
 (
 UKHSA_centre VARCHAR(255),
@@ -13,47 +11,15 @@ secondary_care FLOAT NOT NULL,
 total FLOAT NOT NULL
 );
 
-LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/CLEAN Consumption Location - ESPAUR-2024-to-2025.csv'
-INTO TABLE consumption_location
-FIELDS TERMINATED BY ','
-LINES TERMINATED BY '\r\n'
-IGNORE 1 LINES
-(UKHSA_centre, year, primary_care, secondary_care, total);
+SELECT * FROM amr_2024;
 
-CREATE TABLE amr_location
-(
-region VARCHAR(255),
-year INT NOT NULL,
-amr_raw FLOAT NOT NULL,
-amr_per_100000 FLOAT NOT NULL,
-infections_raw FLOAT NOT NULL,
-infections_per_100000 FLOAT NOT NULL
-);
+ALTER TABLE amr_2024
+RENAME COLUMN `Estimated number of resistant bacteraemia infections` to `AMR Total 2024`;
 
-CREATE TABLE amr_location_2020 
-(
-region VARCHAR(255),
-phe_centre VARCHAR(255),
-year INT NOT NULL,
-amr_per_100000 FLOAT NOT NULL,
-incidence_per_100000 FLOAT NOT NULL
-);
+SELECT cons.Region, cons.year, cons.`Total Consumption`, amr.`AMR Total 2024`
+FROM consumption_location as cons
+JOIN amr_2024 as amr
+ON cons.Region = amr.Region
+WHERE cons.year = 2023;
 
-LOAD DATA INFILE "C:\Users\cmat0\OneDrive\Coding\Antibiotics\CLEAN AMR 2020.csv"
-INTO TABLE amr_location_2020
-FIELDS TERMINATED BY ','
-LINES TERMINATED BY '\r\n'
-IGNORE 1 LINES
-(region, phe_centre, amr_rate_per_100000,total_incidence_per_100000)
-SET year = 2020;
-
-LOAD DATA INFILE "C:\Users\cmat0\OneDrive\Coding\Antibiotics\CLEAN AMR 2021.csv"
-INTO TABLE amr_location
-FIELDS TERMINATED BY ','
-LINES TERMINATED BY '\r\n'
-IGNORE 1 LINES
-(region, amr_per_100000,infections_raw, infections_per_100000)
-SET year = 2021;
-
-SELECT * FROM consumption_location
-WHERE year = 2021;
+SELECT * FROM amr_2023;
